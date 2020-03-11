cce59964eef269362e5816b60a7871f7d9f67285
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2020-03-11 23:52:31 +0900

    Bump react-styleguidist up to 11.0.1

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   components: "src/components/**/*.{ts,tsx}",
   webpackConfig: require("./webpack.config"),
-  propsParser: require("react-docgen-typescript").withDefaultConfig({
-    savePropValueAsString: true
-  }).parse,
   // it is for making the Github pages.
   styleguideDir: "docs",
 };
